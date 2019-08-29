0290277b894efab285b8f8420cb9e00ef2356788
Djalma Beraldo <djalma.batista@indt.org.br>
2019-08-29 14:58:19 -0400

    Fixed eslint-utils and improved build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,10 +6,9 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
+            salad: 0,
+            bacon: 0,
+            meat: 1
         }
     }
     render () {
